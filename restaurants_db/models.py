4883fee0634b6615c791e30b24d24ee0013d6002
GCP VM <slmaaa_greenhub_gmail_com@greenhub-vm.asia-east2-a.c.sonic-name-346213.internal>
2022-04-11 17:16:05 +0000

    Initial commit

---
+++ restaurants_db/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+import datetime
+import uuid
+
+# Create your models here.
+class Restaurant(models.Model):
+    REGION_CHOICES = [("NT","New Territories"),
+    ("KLN","Kowloon"),
+    ("HK","Hong Kong Island"),]
+
+    DISTRICT_CHOICES =[
+        ("CW","Central and Western"),
+        ("EA","Eastern"),
+        ("IS","Islands"),
+        ("KC","Kowloon City"),
+        ("KI","Kwai Tsing"),
+        ("KU","Kwun Tong"),
+        ("NO","North"),
+        ("SK","Sai Kung"),
+        ("SS","Sham Shui Po"),
+        ("ST","Sha Tin"),
+        ("SO","Southern"),
+        ("TP","Tai Po"),
+        ("TW","Tsuen Wan"),
+        ("TM","Tuen Mun"),
+        ("WC","Wan Chai"),
+        ("WT","Wong Tai Sin"),
+        ("YT","Yau Tsim Mong"),
+        ("YL","Yuen Long")]
+
+
+    name = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    region = models.CharField(max_length=3,choices=REGION_CHOICES,default="NT")
+    district = models.CharField(max_length=2,choices=DISTRICT_CHOICES,default="SK")
+
+    cuisine = models.CharField('cuisine type',max_length=100)
+
+    price_range_low = models.IntegerField(default=0)
+    price_range_high = models.IntegerField(default=1000)
+
+    open_rice_url = models.URLField()
+
+    lat = models.FloatField('latitude')
+    lng = models.FloatField('longitude')
+
+    business_hours_from = models.TimeField(default=datetime.time(10,0))
+    business_hours_to = models.TimeField(default=datetime.time(21,0))
+
+    def __str__(self):
+        return self.name
---
+++ restaurants_db/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('all',views.get_all, name='All')
+]
