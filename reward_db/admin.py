657885f98d43e24ecb18637bffe0600f1825aa2a
GCP VM <slmaaa_greenhub_gmail_com@greenhub-vm.asia-east2-a.c.sonic-name-346213.internal>
2022-04-12 20:53:16 +0000

    udpate

---
+++ reward_db/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Reward,Reward_Record
+
+# Register your models here.
+admin.site.register(Reward)
+admin.site.register(Reward_Record)
---
+++ reward_db/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from user_db.models import User
+from restaurants_db.models import Restaurant
+import uuid
+
+# Create your models here
+
+class Reward(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    provider = models.CharField(max_length=100)
+    illustration = models.ImageField(upload_to='rewards')
+    cost = models.IntegerField('G-cashed needed to redeem',default = 1000)
+
+class Coupon(Reward):
+    discount = models.IntegerField(default = 3)
+    min_consume = models.IntegerField(default = 20)
+
+class Reward_Record (models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE)
+    redeem_id = models.UUIDField()
+    status = models.CharField("ACTIVE, USED",default="ACTIVE", max_length= 5)
+    redeem_date = models.DateTimeField()
+    used_date =models.DateTimeField()
+
+    
+    
